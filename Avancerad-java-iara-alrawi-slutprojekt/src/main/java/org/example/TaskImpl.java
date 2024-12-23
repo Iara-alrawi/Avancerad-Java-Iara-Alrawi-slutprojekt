0798ea17c1c478c09041f0b096b325c11cfd6727
iara alrawi <164043505+Iara-alrawi@users.noreply.github.com>
2024-12-23 16:43:41 +0100

    Add files via upload

---
+++ Avancerad-java-iara-alrawi-slutprojekt/src/main/java/org/example/TaskImpl.java
@@ -0,0 +1,35 @@
+package org.example;
+
+public class TaskImpl implements Task {
+    private int id;
+    private String title;
+    private String description;
+    private boolean updated;
+
+    public TaskImpl(int id, String title, String description, boolean updated) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.updated = updated;
+    }
+
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public boolean isUpdated() {
+        return updated;
+    }
+}
