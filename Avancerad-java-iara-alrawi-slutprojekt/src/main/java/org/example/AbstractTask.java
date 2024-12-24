e85de63ed92ea8869fb80a4498ad5ae395117351
iara alrawi <164043505+Iara-alrawi@users.noreply.github.com>
2024-12-24 02:45:22 +0100

    Delete Avancerad-java-iara-alrawi-slutprojekt/src/main/java/org/example directory

--- Avancerad-java-iara-alrawi-slutprojekt/src/main/java/org/example/AbstractTask.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-public abstract class AbstractTask {
-    public AbstractTask(int id, String title, String description, boolean updated) {
-    }
-
-    public abstract Integer getTaskId();
-}
